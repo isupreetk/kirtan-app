6256f957b4eda7430d9e861c621676ea0b9c0cfc
supreetkaur <supreet97@gmail.com>
2024-01-10 14:11:30 -0800

    used refs to get values from input search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import "./SearchBar.scss";
 import CloseButton from "../../assets/images/close-line-icon.png";
+import { useRef } from 'react';
 
 const SearchBar = () => {
+
+  let inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearch = () => {
+    console.log("inputRef", inputRef.current?.value);
+  }
+
+  const resetSearch = () => {
+    // if (inputRef.current) {
+    //   inputRef.current.value = "";
+    // }
+    /* replaced the if statement above with the following line */
+      inputRef.current!.value = "";
+  }
+
     return (
         <div className="searchbar">
           <form className="searchbar__form">
             <input
               name="searchInput"
+              ref={inputRef} type="text" 
               className="searchbar__input"
               placeholder="Search by Shabad, Sevadar or Samagam"
+              // defaultValue={urlSearchString !== null ? `${urlSearchString}` : ""}
+              defaultValue="" // comment this when uncomment above line
+              onChange={handleSearch}
             ></input>
             <img
               src={CloseButton}
               alt="clear search"
               className="searchbar__close-button"
+              onClick={resetSearch}
             ></img>
           </form>
         </div>
